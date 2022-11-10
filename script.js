a9ecc8d46ca80757c32aeb1415df227b59c626f1
Yuriy Yurich <63981542+shzFas@users.noreply.github.com>
2022-11-10 10:03:42 +0600

    Комент

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ let imgFace;
 let select = -1;
 
 function preload() {
- imgMask = loadImage("https://img2.akspic.ru/previews/9/0/9/8/6/168909/168909-ballonchik-graffiti-ulichnoe_iskusstvo-svet-purpur-500x.jpg");
- imgFace = loadImage("https://img2.akspic.ru/previews/9/0/9/8/6/168909/168909-ballonchik-graffiti-ulichnoe_iskusstvo-svet-purpur-500x.jpg");
+ imgMask = loadImage("https://img2.akspic.ru/previews/9/0/9/8/6/168909/168909-ballonchik-graffiti-ulichnoe_iskusstvo-svet-purpur-500x.jpg"); // ссылка на изображение которое будет отображаться
+ imgFace = loadImage("https://img2.akspic.ru/previews/9/0/9/8/6/168909/168909-ballonchik-graffiti-ulichnoe_iskusstvo-svet-purpur-500x.jpg");// ссылка на изображение которое будет отображаться
 }
 
 function setup() {
@@ -30,6 +30,7 @@ function setup() {
     const selectList = ['Mask', 'Face'];
     selected.option('Select filter', -1);
 
+    // вывод инпут меню для конфигурации через lenght
     for(let i = 0; i < selectList.length; i++){
         selected.option(selectList[i], i)
     }
@@ -55,24 +56,26 @@ function draw() {
     }
 }
 
+// Отрисовка 1 изображения
 function drawMask() {
     const positions = faceTracker.getCurrentPosition();
     if(positions !== false) {
         push();
         const wx = Math.abs(positions[13][0] -positions[1][0]) * 1.2;
-        const wy = Math.abs(positions[7][1] - Math.min(positions[16][1], positions[20][1])) * 1.2;
+        const wy = Math.abs(positions[7][1] - Math.min(positions[16][1], positions[20][1])) * 1.2; // https://github.com/auduno/clmtrackr - в зависимости от точек на лице будет растянута маска(картинка)
         translate(-wx/2, -wy/2)
         image(imgMask, positions[62][0], positions[62][1], wx, wy);
         pop();
     }
 }
 
+// Отрисовка 2 изображения
 function drawFace() {
     const positions = faceTracker.getCurrentPosition();
     if(positions !== false) {
         push();
         const wx = Math.abs(positions[13][0] -positions[1][0]) * 1.2;
-        const wy = Math.abs(positions[7][1] - Math.min(positions[16][1], positions[20][1])) * 1.2;
+        const wy = Math.abs(positions[7][1] - Math.min(positions[16][1], positions[20][1])) * 1.2; 
         translate(-wx/2, -wy/2)
         image(imgFace, positions[62][0], positions[62][1], wx, wy);
         pop();
